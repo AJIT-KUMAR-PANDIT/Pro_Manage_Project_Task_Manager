cb2f9fd53c29ee4e6fdd78469de04bec8fb43202
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2024-02-20 22:43:15 +0530

    update 1.3.1.2

--- client/src/Components/Dashboard/AddModalElement/AddModalElement.jsx
+++ client/src/Components/Dashboard/AddModalElement/AddModalElement.jsx
@@ -1,25 +1,23 @@
 import React, { useState, forwardRef } from 'react';
-import StylesAddModalElement from './AddModalElement.module.css'
+import StylesAddModalElement from './AddModalElement.module.css';
 import ModalTaskList from '../ModalTaskList/ModalTaskList';
-import { useDispatch } from 'react-redux'
-import { closeModal1 } from '../../../Redux/slice'
+import { useDispatch } from 'react-redux';
+import { closeModal1 } from '../../../Redux/slice';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import axios from 'axios';
-import {Url} from '../../../Utils/Url';
-
+import { Url } from '../../../Utils/Url';
 
 const AddModalElement = () => {
-
     const baseUrl = Url();
-
-    const [taskList, setTaskList] = useState([])
-
+    const [taskList, setTaskList] = useState([]);
+    const [selectedPriority, setSelectedPriority] = useState(null); // State to store selected priority
     const dispatch = useDispatch();
+    const [startDate, setStartDate] = useState(null);
+
     const handleCloseModal = () => {
         dispatch(closeModal1());
     };
-
 
     const handleAddTask = () => {
         // Add a new task to the task list
@@ -31,70 +29,81 @@ const AddModalElement = () => {
         setTaskList(taskList.filter((_, i) => i !== index));
     };
 
-
-
-    // due datepicker start
-
-    
-    const [startDate, setStartDate] = useState(null);
+    const handlePriorityClick = (priority) => {
+        // Set the selected priority when a button is clicked
+        setSelectedPriority(priority);
+    };
 
     const DateInput = forwardRef(({ value, onClick }, ref) => (
-      <button
-      className={StylesAddModalElement.button1}
-        onClick={onClick}
-        ref={ref}
-      >
-        {value || "Select Due Date"} {/* Display placeholder if value is empty */}
-      </button>
+        <button
+            className={StylesAddModalElement.button1}
+            onClick={onClick}
+            ref={ref}
+        >
+            {value || "Select Due Date"} {/* Display placeholder if value is empty */}
+        </button>
     ));
-  
 
-    // due datepicker end
-
+    const handleSave = () => {
+        const title = document.getElementById('taskTitle').value;
+        const priority = selectedPriority; // Use the selected priority from state
+        const checklist = taskList.map(task => ({ taskName: task.props.children }));
+        const dueDate = startDate;
+        
+        // Make a POST request to the backend server
+        axios.post(`${baseUrl}/api/addtask`, { title, priority, checklist, dueDate })
+            .then(response => {
+                console.log('Task added successfully:', response.data);
+                // Optionally, you can perform any additional actions after successful save
+            })
+            .catch(error => {
+                console.error('Error adding task:', error);
+                // Handle error
+            });
+    };
 
     return (
         <>
             <div className={StylesAddModalElement.addModalElement}>
                 <div className={StylesAddModalElement.title}>Title<span className={StylesAddModalElement.asterisk}> *</span></div>
                 <div>
-                    <input type='text' className={StylesAddModalElement.inputTitle} placeholder='Enter Task Title' />
+                    <input id="taskTitle" type='text' className={StylesAddModalElement.inputTitle} placeholder='Enter Task Title' />
+                </div>
+                <br />
+                <div style={{ display: 'flex'}}>
+                    
+                    <span>Select Priority<span className={StylesAddModalElement.asterisk}>*</span></span>
+                    <div className={StylesAddModalElement.priorityOptions}>
+                        <button value="HIGH PRIORITY" className={StylesAddModalElement.addPriority} onClick={() => handlePriorityClick("HIGH PRIORITY")}><img src='Assets/high.svg' alt='addPriority' />&nbsp;&nbsp;HIGH PRIORITY</button>
+                        <button value="MODERATE PRIORITY" className={StylesAddModalElement.addPriority} onClick={() => handlePriorityClick("MODERATE PRIORITY")}><img src='Assets/moderate.svg' alt='addPriority' />&nbsp;&nbsp;MODERATE PRIORITY</button>
+                        <button value="LOW PRIORITY" className={StylesAddModalElement.addPriority} onClick={() => handlePriorityClick("LOW PRIORITY")}><img src='Assets/low.svg' alt='addPriority' />&nbsp;&nbsp;LOW PRIORITY</button>
+                    </div>
                 </div>
                 <div>
                     <br />
-                    <span>Select Priority
-                        <span className={StylesAddModalElement.asterisk}>*</span></span><span className={StylesAddModalElement.addPriority}><img src='Assets/high.svg' alt='addPriority' />&nbsp;&nbsp;HIGH PRIORITY</span>
-                    <span className={StylesAddModalElement.addPriority}><img src='Assets/moderate.svg' alt='addPriority' />&nbsp;&nbsp;MODERATE PRIORITY</span>
-                    <span className={StylesAddModalElement.addPriority}><img src='Assets/low.svg' alt='addPriority' />&nbsp;&nbsp;LOW PRIORITY
-                    </span>
-                </div>
-                <div>
-                    <br />
-                    <span>Checklist (1/3)
-                        <span className={StylesAddModalElement.asterisk}>*</span>
-                    </span>
+                    <span>Checklist (1/3)<span className={StylesAddModalElement.asterisk}>*</span></span>
                 </div>
                 <div className={StylesAddModalElement.checklist}>
                     <ModalTaskList />
                 </div>
                 <br />
                 <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <div style={{ display: 'flex', alignContent: 'center',alignItems: 'center' }}>
+                    <div style={{ display: 'flex', alignContent: 'center', alignItems: 'center' }}>
                         <DatePicker
                             selected={startDate}
                             onChange={(date) => setStartDate(date)}
                             customInput={<DateInput />}
                             placeholderText='Select Due Date'
                         />
-                        {console.log(startDate)}
                     </div>
                     <div style={{ display: 'flex', gap: '21px' }}>
                         <button className={StylesAddModalElement.cancel} onClick={() => handleCloseModal()}>Cancel</button>
-                        <button className={StylesAddModalElement.save}>Save</button>
+                        <button className={StylesAddModalElement.save} onClick={handleSave}>Save</button>
                     </div>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
 export default AddModalElement;
