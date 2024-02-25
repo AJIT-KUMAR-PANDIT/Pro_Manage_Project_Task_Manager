3dd0b4c428501f2dd464b079a5b4a446d70d6118
AJIT-KUMAR-PANDIT <ajitkumarpandit1999@gmail.com>
2024-02-25 19:02:00 +0530

    update 7.7.3

--- client/src/ProtectedRoutes/ProtectedRoutes.js
+++ client/src/ProtectedRoutes/ProtectedRoutes.js
@@ -2,12 +2,20 @@ import { Route, Navigate } from 'react-router-dom';
 import { useTimer } from 'react-timer-hook';
 
 const ProtectedRoutes = ({ children }) => {
-const isAuthenticated = localStorage.getItem('das');
+  const isAuthenticated = localStorage.getItem('das');
 
-const expiryTimestamp = new Date();
-expiryTimestamp.setSeconds(expiryTimestamp.getSeconds() + 3600);
+  const expiryTimestamp = new Date();
+  expiryTimestamp.setSeconds(expiryTimestamp.getSeconds() + 3600);
 
- useTimer({ expiryTimestamp, onExpire: () =>{ localStorage.removeItem('das') ; <Navigate to="/" replace /> } });
+  useTimer({
+    expiryTimestamp, onExpire: () => {
+      localStorage.removeItem('das');
+      localStorage.removeItem('token');
+      localStorage.removeItem('id');
+      localStorage.removeItem('name');
+      <Navigate to="/" replace />
+    }
+  });
 
   return isAuthenticated ? (
     children
